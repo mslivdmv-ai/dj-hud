c2239f067847d6ab0e6e2ca52b468e5ace0c81f6
mslivdmv-ai <mslivdmv@gmail.com>
2026-07-29 13:58:15 -0400

    Add client/player.lua (player info sender)

---
+++ client/player.lua
@@ -0,0 +1,83 @@
+-------------------------------------
+-- DG-HUD Player Information
+-------------------------------------
+
+local ESX = exports['es_extended']:getSharedObject()
+
+local PlayerData = {}
+
+RegisterNetEvent('esx:playerLoaded', function(xPlayer)
+    PlayerData = xPlayer
+end)
+
+RegisterNetEvent('esx:setJob', function(job)
+    PlayerData.job = job
+end)
+
+CreateThread(function()
+
+    while true do
+
+        Wait(1000)
+
+        if not LocalPlayer.state.isLoggedIn then
+            goto continue
+        end
+
+        local jobName = "Unemployed"
+        local jobGrade = ""
+        local duty = "Off Duty"
+
+        if PlayerData.job then
+            jobName = PlayerData.job.label or PlayerData.job.name
+            jobGrade = PlayerData.job.grade_label or ""
+
+            if PlayerData.job.onDuty ~= nil then
+                duty = PlayerData.job.onDuty and "On Duty" or "Off Duty"
+            else
+                duty = "On Duty"
+            end
+        end
+
+        local cash = exports.ox_inventory:Search('count', 'money') or 0
+        local bank = 0
+        local dirty = exports.ox_inventory:Search('count', 'black_money') or 0
+
+        -- Replace this export if you're using a different banking resource
+        if exports['Renewed-Banking'] then
+            local account = exports['Renewed-Banking']:getAccountMoney('bank')
+
+            if account then
+                bank = account
+            end
+        end
+
+        SendNUIMessage({
+
+            action = "player",
+
+            id = GetPlayerServerId(PlayerId()),
+
+            cash = cash,
+
+            bank = bank,
+
+            dirty = dirty,
+
+            job = jobName,
+
+            grade = jobGrade,
+
+            duty = duty,
+
+            online = #GetActivePlayers(),
+
+            maxPlayers = GetConvarInt("sv_maxclients", 64)
+
+        })
+
+        ::continue::
+
+    end
+
+end)
