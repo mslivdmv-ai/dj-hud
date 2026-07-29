2df1cff5d2efc26eee427bde7affc7b7a91bbbfe
mslivdmv-ai <mslivdmv@gmail.com>
2026-07-29 14:17:05 -0400

    Update fxmanifest: explicit client_scripts list including client/settings.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,4 +1,52 @@
+fx_version 'cerulean'
+game 'gta5'
+
+lua54 'yes'
+
+author 'DG Development'
+description 'DG-HUD - Modern ESX HUD'
+version '1.0.0'
+
+shared_scripts {
+    '@ox_lib/init.lua',
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua',
+    'client/status.lua',
+    'client/voice.lua',
+    'client/vehicle.lua',
+    'client/compass.lua',
+    'client/player.lua',
+    'client/minimap.lua',
+    'client/settings.lua'
+}
+
 server_scripts {
     '@oxmysql/lib/MySQL.lua',
     'server/main.lua'
+}
+
+ui_page 'web/index.html'
+
+files {
+    'web/index.html',
+    'web/style.css',
+    'web/script.js',
+    'web/assets/*.*',
+    'locales/*.json'
+}
+
+dependencies {
+    'es_extended',
+    'ox_lib',
+    'ox_inventory',
+    'ox_target',
+    'pma-voice',
+    'ox_fuel'
+}
+
+escrow_ignore {
+    'config.lua'
 }
